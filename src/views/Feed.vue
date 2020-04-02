65c3086b3c469543c4a620d527de363f92ce1832
AndyT2503 <60542561+AndyT2503@users.noreply.github.com>
2020-04-02 19:13:39 +0700

    update search box

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -4,23 +4,49 @@
       <div class="section profile-content">
         <div class="container">
           <div class="md-layout">
-            <div class="md-layout-item md-size-70 md-small-size-100">
+            <div
+              class="md-layout-item md-size-30 md-small-size-100 order-lg-2 order-xl-2 order-sm-1 order-md-1"
+            >
+              <form
+                action="#"
+                method="post"
+                novalidate="novalidate"
+                style="margin-left:20px;"
+                class="searchBox"
+              >
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="row" style="margin-top:20px">
+                      <div class="col-lg-7 col-md-7 col-sm-12 p-0 searchItem">
+                        <input type="text" class="form-control search-slt" placeholder="Tìm kiếm" />
+                      </div>
+                      <div class="col-lg-7 col-md-7 col-sm-12 p-0 searchItem">
+                        <select class="form-control search-slt" id="exampleFormControlSelect1">
+                          <option>Tất cả</option>
+                          <option>Người đăng</option>
+                          <option>Hình ảnh</option>
+                          <option>Địa điểm</option>
+                        </select>
+                      </div>
+                      <div class="col-lg-2 col-md-2 col-sm-12 p-0 searchItem">
+                        <button type="button" class="btn btn-danger wrn-btn">Search</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div
+              class="md-layout-item md-size-70 md-small-size-100 order-lg-1 order-xl-1 order-sm-2 order-md-2"
+            >
               <div v-for="index in 10" :key="index" class="my-3">
                 <div class="md-layout md-layout-item activity-box">
-                  <div
-                    class="md-layout-item md-size-100 d-flex activity-box--header"
-                  >
+                  <div class="md-layout-item md-size-100 d-flex activity-box--header">
                     <div class="profile-photo">
-                      <img
-                        class="rounded-circle"
-                        src="@/assets/img/faces/avatar.jpg"
-                      />
+                      <img class="rounded-circle" src="@/assets/img/faces/avatar.jpg" />
                     </div>
                     <div class="ml-3">
-                      <router-link class="h5" :to="{ name: 'profile' }">
-                        Jenifer Law
-                      </router-link>
-                      Write a post
+                      <router-link class="h5" :to="{ name: 'profile' }">Jenifer Law</router-link>Write a post
                       <p class="text-muted">2 hours ago</p>
                     </div>
                   </div>
@@ -34,17 +60,17 @@
                   </div>
                   <div class="my-1">
                     <md-button v-if="true" class="md-rose md-sm md-round">
-                      <md-icon class="mr-1">favorite</md-icon> 20
+                      <md-icon class="mr-1">favorite</md-icon>20
                     </md-button>
                     <md-button v-if="true" class="md-info md-sm md-round ml-3">
-                      <md-icon class="mr-1">comment</md-icon> 20
+                      <md-icon class="mr-1">comment</md-icon>20
                     </md-button>
                   </div>
                   <hr class="w-100 my-1" />
                   <div class="d-flex mt-1 align-items-center w-100">
                     <!-- <div class="profile-photo--comment mr-3">
                       <img class="rounded-circle" src="@/assets/img/faces/avatar.jpg" />
-                    </div> -->
+                    </div>-->
                     <md-field class="flex-fill">
                       <md-icon>comment</md-icon>
                       <label>Leave a comment</label>
@@ -57,18 +83,12 @@
                       src="@/assets/img/faces/avatar.jpg"
                     />
                     <div class="flex-fill ml-3">
-                      <router-link class="h5" :to="{ name: 'profile' }">
-                        Jenifer Law
-                      </router-link>
-                      Can i take a picture?
+                      <router-link class="h5" :to="{ name: 'profile' }">Jenifer Law</router-link>Can i take a picture?
                       <p class="text-muted m-0">2 hours ago</p>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="md-layout-item md-size-30 md-small-size-100">
-              Box search
             </div>
           </div>
         </div>
@@ -149,6 +169,14 @@ export default {
       height: 40px;
       width: 40px;
     }
+  }
+}
+.searchItem{
+  margin: 1px 1px 1px 1px;
+}
+@media screen and (min-width: 1000px) {
+  .searchBox {
+    position: fixed;
   }
 }
 </style>
